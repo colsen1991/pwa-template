75c00700eaf6a21e0bcb46eec8ec8b4772936fce
Christer Olsen <colsen1991@gmail.com>
2017-09-19 13:08:42 +0200

    Dato shouldn't crash after init.

--- template/dato.config.js
+++ template/dato.config.js
@@ -1,5 +1,7 @@
 module.exports = (dato, root) => {
   root.directory('static/data', (dataDir) => {
-
+    dataDir.createPost('test.json', 'json', {
+      frontmatter: dato.tests.map(test => test.toMap())
+    })
   })
 }
